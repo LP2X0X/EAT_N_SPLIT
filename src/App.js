49d6185aaadedfd2ce156c95462b4875212bfa70
long.pham <phamquanglong.work@gmail.com>
2025-08-17 18:09:15 +0700

    Add key to FormSplitBill so it reset the state between selection

--- src/App.js
+++ src/App.js
@@ -94,6 +94,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </main>
